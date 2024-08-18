a36a1de2b6c38d6339b2b788d707449d2e8b9831
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-08-18 20:53:08 +0200

    use latest crypto3

---
+++ evm-assigner.nix
@@ -0,0 +1,49 @@
+{ lib,
+  stdenv,
+  src_repo,
+  ninja,
+  pkg-config,
+  cmake,
+  boost183,
+  # We'll use boost183 by default, but you can override it
+  boost_lib ? boost183,
+  gdb,
+  ethash,
+  intx,
+  gtest,
+  crypto3,
+  enableDebugging,
+  enableDebug ? false,
+  runTests ? false,
+  }:
+let
+  inherit (lib) optional;
+in stdenv.mkDerivation rec {
+  name = "evm-assigner";
+
+  src = src_repo;
+
+  nativeBuildInputs = [ cmake ninja pkg-config ] ++ (lib.optional (!stdenv.isDarwin) gdb);
+
+  # enableDebugging will keep debug symbols in boost
+  propagatedBuildInputs = [ (if enableDebug then (enableDebugging boost_lib) else boost_lib) ];
+
+  buildInputs = [crypto3 ethash intx gtest];
+
+  cmakeFlags =
+  [
+      (if runTests then "-DBUILD_TESTS=TRUE" else "")
+      (if runTests then "-DCMAKE_ENABLE_TESTS=TRUE" else "")
+      (if enableDebug then "-DBUILD_ASSIGNER_TESTS=TRUE" else "")
+      (if enableDebug then "-DCMAKE_BUILD_TYPE=Debug" else "-DCMAKE_BUILD_TYPE=Release")
+      (if enableDebug then "-DCMAKE_CXX_FLAGS=-ggdb" else "")
+      "-G Ninja"
+  ];
+
+  doCheck = runTests;
+
+  shellHook = ''
+    PS1="\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+    echo "Welcome to evm-assigner development environment!"
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nix flake for EVM-assigner";
+  description = "Nix flake for evm-assigner";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
@@ -14,142 +14,65 @@
     nil-crypto3 = {
       url = "https://github.com/NilFoundation/crypto3";
       type = "git";
-      rev = "3bd5b8df2091274abaa28fd86b9e3e89d661b95a";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nix-3rdparty.follows = "nix-3rdparty";
-      };
-    };
-    nil-zkllvm-blueprint = {
-      url = "https://github.com/NilFoundation/zkllvm-blueprint";
-      type = "git";
-      submodules = true;
-      rev = "73d6a40e39b6b6fc7b1c84441e62337206dc0815";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-        nil-crypto3.follows = "nil-crypto3";
       };
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      nix-3rdparty,
-      ...
-    }@inputs:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, nil-crypto3, flake-utils, nix-3rdparty }:
+    (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          overlays = [ nix-3rdparty.overlays.${system}.default ];
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
+        stdenv = pkgs.llvmPackages_16.stdenv;
+        crypto3 = nil-crypto3.packages.${system}.crypto3;
+        intx = nix-3rdparty.packages.${system}.intx;
 
-        # Add more complicated logic here if you need to have debug packages
-        resolveInput = input: input.packages.${system}.default;
-
-        nil-inputs = with inputs; [
-          nil-crypto3
-          nil-zkllvm-blueprint
-        ];
-
-        nil-packages = map resolveInput nil-inputs;
-
-        defaultNativeBuildInputs = with pkgs; [
-          cmake
-          ninja
-        ];
-
-        defaultCheckInputs = [ pkgs.gtest ];
-
-        commonBuildInputs = [ pkgs.boost ];
-
-        inputsToPropagate =
-          {
-            enableDebug ? false,
-          }:
-          let
-            deps = with pkgs; [
-              (intx.override { inherit enableDebug; })
-              ethash
-            ];
-          in
-          deps ++ nil-packages;
-
-        devInputs = [ pkgs.clang_17 ];
-
-        makePackage =
-          {
-            enableDebug ? false,
-          }:
-          pkgs.gcc13Stdenv.mkDerivation {
-            name = "EVM-assigner" + "${if enableDebug then "-debug" else ""}";
-
-            nativeBuildInputs = defaultNativeBuildInputs;
-
-            buildInputs = commonBuildInputs;
-
-            propagatedBuildInputs = inputsToPropagate { inherit enableDebug; };
-
-            src = self;
-
-            cmakeBuildType = if enableDebug then "Debug" else "Release";
-
-            doCheck = false;
-            dontStrip = enableDebug;
-          };
-
-        makeTests =
-          {
-            enableDebug ? false,
-          }:
-          (makePackage { inherit enableDebug; }).overrideAttrs (
-            finalAttrs: previousAttrs: {
-              name = previousAttrs.name + "-tests";
-
-              checkInputs = defaultCheckInputs;
-
-              cmakeFlags = [ "-DBUILD_ASSIGNER_TESTS=TRUE" ];
-
-              dontInstall = true;
-
-              doCheck = true;
-
-              GTEST_OUTPUT = "xml:${placeholder "out"}/test-reports/";
-            }
-          );
-
-        makeDevShell =
-          {
-            enableDebug ? false,
-          }:
-          pkgs.mkShell {
-            nativeBuildInputs = defaultNativeBuildInputs;
-            buildInputs =
-              commonBuildInputs ++ inputsToPropagate { inherit enableDebug; } ++ defaultCheckInputs ++ devInputs;
-
-            shellHook = ''
-              echo "evm-assigner dev ${if enableDebug then "debug" else "release"} environment activated"
-            '';
-          };
-      in
-      {
+      in {
         packages = rec {
-          release = makePackage { };
-          debug = makePackage { enableDebug = true; };
-          default = release;
+          evm-assigner = (pkgs.callPackage ./evm-assigner.nix {
+            src_repo = self;
+            crypto3 = crypto3;
+            intx = intx;
+          });
+          evm-assigner-debug = (pkgs.callPackage ./evm-assigner.nix {
+            src_repo = self;
+            crypto3 = crypto3;
+            intx = intx;
+            enableDebug = true;
+          });
+          evm-assigner-debug-tests = (pkgs.callPackage ./evm-assigner.nix {
+            src_repo = self;
+            crypto3 = crypto3;
+            intx = intx;
+            enableDebug = true;
+            runTests = true;
+          });
+          default = evm-assigner-debug-tests;
         };
-        checks = {
-          default = makeTests { };
+        checks = rec {
+          gcc = (pkgs.callPackage ./evm-assigner.nix {
+            src_repo = self;
+            crypto3 = crypto3;
+            intx = intx;
+            runTests = true;
+          });
+          clang = (pkgs.callPackage ./evm-assigner.nix {
+            stdenv = pkgs.llvmPackages_18.stdenv;
+            src_repo = self;
+            crypto3 = crypto3;
+            intx = intx;
+            runTests = true;
+          });
+          all = pkgs.symlinkJoin {
+            name = "all";
+            paths = [ gcc clang ];
+          };
+          default = all;
         };
-        devShells = {
-          default = makeDevShell { };
-          debug = makeDevShell { enableDebug = true; };
-        };
-      }
-    );
+      }));
 }
+
+# `nix flake -L check` to run all tests (-L to output build logs)
+# `nix flake show` to show derivations tree
+# If build fails due to OOM, run `export NIX_CONFIG="cores = 2"` to set desired parallel level
