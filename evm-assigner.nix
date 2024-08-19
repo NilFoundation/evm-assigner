288b7de387343391902d38d80e9fe3d6fa6e03bb
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-08-19 22:37:08 +0200

    fix linkage errors in test - add boost::log

--- evm-assigner.nix
+++ evm-assigner.nix
@@ -34,7 +34,7 @@ in stdenv.mkDerivation rec {
   [
       (if runTests then "-DBUILD_TESTS=TRUE" else "")
       (if runTests then "-DCMAKE_ENABLE_TESTS=TRUE" else "")
-      (if enableDebug then "-DBUILD_ASSIGNER_TESTS=TRUE" else "")
+      (if runTests then "-DBUILD_ASSIGNER_TESTS=TRUE" else "")
       (if enableDebug then "-DCMAKE_BUILD_TYPE=Debug" else "-DCMAKE_BUILD_TYPE=Release")
       (if enableDebug then "-DCMAKE_CXX_FLAGS=-ggdb" else "")
       "-G Ninja"
